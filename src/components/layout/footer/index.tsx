d42af9c83a324c6d0c69e3380f58102ad75d73ec
leewwwhui <leewwhui@gmail.com>
2024-11-16 02:42:02 +0800

    💄 style: footer spelator

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -3,16 +3,16 @@ import { FooterNav } from "./footer-nav";
 
 export const Footer = () => {
   return (
-    <div className="shadow-card bg-white border rounded-lg mt-32 flex flex-col gap-12 items-center justify-center py-16 px-8 md:px-28">
+    <div className="shadow-card bg-white border rounded-lg mt-32 flex flex-col gap-12 items-center justify-center py-16 px-[30px] md:px-28">
       <FooterNav />
 
       <div className="w-full flex flex-col gap-7">
-        <div className="py-7 flex flex-col items-center justify-between border-y gap-5 md:flex-row">
-          <div className="flex items-center gap-4">
+        <div className="py-7 flex flex-col items-center justify-between border-y gap-5 lg:flex-row">
+          <div className="flex items-center text-sm lg:text-[18px] gap-3 h-8">
             <p>Terms of Service</p>
-            <div className="h-1/2 border-1"></div>
+            <div className="h-full border-1"></div>
             <p>Privacy Policy</p>
-            <div className="h-1/2 border-1"></div>
+            <div className="h-full border-1"></div>
             <p>Cookie Policy</p>
           </div>
 
